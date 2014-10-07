b3b25476c090c091948de3f970cf3685f95f8f34
Matveev Roman <mr@htc-cs.com>
2014-10-07 10:04:03 +0400

    Initial Commit

---
+++ StackOverflowQuestions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackOverflowQuestions
+//
+//  Created by Aztec on 07.10.14.
+//  Copyright (c) 2014 Aztec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StackOverflowQuestions/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StackOverflowQuestions
+//
+//  Created by Aztec on 07.10.14.
+//  Copyright (c) 2014 Aztec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
